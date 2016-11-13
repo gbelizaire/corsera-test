a5d07fc3fbf54ad9203207c9cd91e7946b5faf7d
Gerrald Belizaire <gbelizaire@gmail.com>
2016-11-13 06:19:31 -0800

    Module1 Assignment

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -9,19 +9,23 @@
        $scope.items ="Mais, du riz, pois, manioc";
        $scope.message ="Test...";
        $scope.nbItems = 0;
+       $scope.CheckIfTooMuch_Click = false;
       
 
 
       // function CheckIfTooMuch
       $scope.CheckIfTooMuch = function(){
+          $scope.CheckIfTooMuch_Click = true;
        console.log($scope.items);
        $scope.nbItems =NombreItems();
-       if($scope.nbItems<=3){
-           $scope.message = "Enjoy!";
-       }else{
-           $scope.message = "Too much!";
-       }
-       
+            if($scope.nbItems == 0){
+                $scope.message = "Please enter data first";
+            } else if($scope.nbItems<=3){
+                $scope.message = "Enjoy!";
+            }else{
+                $scope.message = "Too much!";
+            }
+            
       }
       function NombreItems(){
           return $scope.items.split(",").length;
