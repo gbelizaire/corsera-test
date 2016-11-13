bff3042d17a427008ee1fc2903a1447984fbbd17
Gerrald Belizaire <gbelizaire@gmail.com>
2016-11-13 00:01:26 -0800

    Angular test Lecture 4

--- lecture4/app.js
+++ lecture4/app.js
@@ -5,6 +5,7 @@
    .controller('myFirstController',function($scope){
        $scope.nom ="Belizaire";
        $scope.prenom = "Gerald";
+       $scope.age = 38;
        console.log('Je suis la et je vis par la grace de Dieu');
    });
    
