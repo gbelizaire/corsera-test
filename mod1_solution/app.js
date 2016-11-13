247e12b9ab27f3586a702c6c2bd83ed0bb72910e
Gerrald Belizaire <gbelizaire@gmail.com>
2016-11-13 06:08:26 -0800

    Module1 Assignment

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -4,24 +4,27 @@
    angular.module('LunchCheck',[])
    .controller('LunchCheckController',LunchCheckCtrl);
 
-
-
    LunchCheckCtrl.$inject =['$scope'];
    function LunchCheckCtrl($scope){
        $scope.items ="Mais, du riz, pois, manioc";
-       console.log('Je suis la ');
        $scope.message ="Test...";
-       $scope.listItems =[];
+       $scope.nbItems = 0;
+      
 
 
       // function CheckIfTooMuch
       $scope.CheckIfTooMuch = function(){
        console.log($scope.items);
-        console.log( $scope.items.split(",").length);
-        $scope.message = $scope.listItems.length.toString();
-        
-      //  console.log( $scope.listItems.length);
-     //   $scope.message = $scope.listItems.length.toString();
+       $scope.nbItems =NombreItems();
+       if($scope.nbItems<=3){
+           $scope.message = "Enjoy!";
+       }else{
+           $scope.message = "Too much!";
+       }
+       
+      }
+      function NombreItems(){
+          return $scope.items.split(",").length;
       }
 
    }
