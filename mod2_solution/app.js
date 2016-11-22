013d59cbd7f664b025d7ad64a1865f9b07fa31f0
Gerrald Belizaire <gbelizaire@gmail.com>
2016-11-21 23:22:44 -0800

    'Solution2'

---
+++ mod2_solution/app.js
@@ -0,0 +1,65 @@
+(function(){
+ 'use strict';
+ /**
+  *  Je dois avoir 2 controlleurs, un pour gerer le 'tobuy list'
+     et un autre pour gerer
+  */
+   angular.module('ShoppingListCheckOff',[])
+   .controller('ToBuyController ',ToBuyController )
+   .controller('AlreadyBoughtController',AlreadyBoughtController)
+   .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+   
+
+  
+   /**
+    *  Creation du premier controlleur
+    */
+   tobuyController.$inject= ['ShoppingListCheckOffService'];
+   function ToBuyController (){
+    var toBuy = this;
+        toBuy.ListeArticles = ShoppingListCheckOffService.ListItemsToBuy();
+   toBuy.message = "tout est ok";
+
+   }
+
+   /**
+    *  Creation du sceond controlleur
+    */  
+    AlreadyBoughtController.$inject= ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(){
+       var alreadyBought = this;
+       alreadyBought.ListeArticles =[];
+    }
+
+
+   /**
+    * Creation d'' une service , un singleton
+    */
+    function ShoppingListCheckOffService(){
+       var Shop = this;
+       Shop.tobuyItems =  [{name: "Chemise", quantity: 10} ,
+                              {name: "T-Shirt", quantity: 10 },
+                              {name: "Pantalon", quantity: 10 },
+                              {name: "Anneaux", quantity: 10 },
+                              {name: "Chaussure", quantity: 10 }
+                              ];
+
+       Shop.boughtItems = [];  
+
+       /** Fonction AddToBoughtItems */
+       Shop.AddToBoughtItems = function(item , quantity){
+         var item ={
+             name:item,
+             quantity :quantity
+         };
+          Shop.tobuyItems.push(item); 
+       } 
+
+       /** fonction ListItemsToBuy */
+       Shop.ListItemsToBuy  = function(){
+           return Shop.tobuyItems;
+       }                  
+
+
+    }
+})();
