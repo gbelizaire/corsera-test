ea752d054ba54c3eda4035f24f2bdfae47cc5143
Gerrald Belizaire <gbelizaire@gmail.com>
2016-12-17 12:40:11 -0500

    Html_Mod4-Solution

--- html_learning/mode4_solution/harder/SpeakGoodBye.js
+++ html_learning/mode4_solution/harder/SpeakGoodBye.js
@@ -17,7 +17,8 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
    byeSpeaker.speak = function(name){
-       //console.log(speakWord+"  "+byeSpeaker.name);
+       
+       document.writeln(speakWord+"  "+name+"<br/>");
        console.log(speakWord+"  "+name);
    };
    
--- html_learning/mode4_solution/harder/SpeakHello.js
+++ html_learning/mode4_solution/harder/SpeakHello.js
@@ -15,7 +15,8 @@
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 	helloSpeaker.speak = function(name){
-	   //console.log(speakWord+"   "+helloSpeaker.name);
+	  
+     document.writeln(speakWord+"   "+name+"<br/>");
      console.log(speakWord+"   "+name);
 	};
 
